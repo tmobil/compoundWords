80d78a79a0391cd097e155531b1c9e8353457aa9
soma gorinta <>
2018-10-10 09:37:55 -0400

    updating the tableview cell

---
+++ CompoundWords/CompoundWords/CompoundWordTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CompoundWordTableViewCell.swift
+//  CompoundWords
+//
+//  Created by soma gorinta on 10/4/18.
+//  Copyright © 2018 soma gorinta. All rights reserved.
+//
+
+import UIKit
+
+class CompoundWordTableViewCell: UITableViewCell {
+  @IBOutlet weak var labelText: UILabel!
+}
